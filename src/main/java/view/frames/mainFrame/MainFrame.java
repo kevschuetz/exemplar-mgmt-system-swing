9c3d1a2d82dff0de4c241c31f784b4dfdc303b6e
kevschuetz <kevin.hotz@gmx.at>
2021-05-18 14:03:05 +0200

    menubar

--- src/main/java/view/frames/mainFrame/MainFrame.java
+++ src/main/java/view/frames/mainFrame/MainFrame.java
@@ -1,20 +1,40 @@
 package view.frames.mainFrame;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 
 
 public class MainFrame extends JFrame{
     private JTabbedPane tabPanel = new JTabbedPane();
-
+    private MenuPanel menuPanel = new MenuPanel();
+    private CMenuBar menuBar = new CMenuBar();
 
 
     public MainFrame() {
+
         addComponents();
     }
 
     void addComponents(){
-        add(tabPanel);
+        setLayout(new GridBagLayout());
+        setJMenuBar(menuBar);
+        int y = 0;
+        int x = 0;
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.BOTH;
+
+        c.weightx = 1;
+        c.weighty = 0.01;
+        c.gridy = y;
+        c.gridx = x;
+        add(menuPanel, c);
+
+
+        y++;
+        c.gridy = y;
+        c.weighty = 0.95;
+        add(tabPanel,c);
     }
 
     public void addTab(String title, Component component){
@@ -25,5 +45,55 @@ public class MainFrame extends JFrame{
         tabPanel.remove(component);
     }
 
+    private class MenuPanel extends JPanel{
+        private JButton exemplarButton;
+        private JButton communityButton;
+        private JButton contributorButton;
 
+        MenuPanel(){
+            setBorder(BorderFactory.createCompoundBorder(
+                    BorderFactory.createTitledBorder(""),
+                    BorderFactory.createEmptyBorder(5, 5, 5, 5)));
+            exemplarButton = new JButton("Exemplars");
+            communityButton = new JButton("Communities");
+            contributorButton = new JButton("Contributors");
+            exemplarButton.setBackground(Color.lightGray);
+            communityButton.setBackground(Color.lightGray);
+            contributorButton.setBackground(Color.lightGray
+            );
+            Border emptyBorder = BorderFactory.createEmptyBorder();
+            exemplarButton.setBorder(emptyBorder);
+            communityButton.setBorder(emptyBorder);
+            contributorButton.setBorder(emptyBorder);
+
+
+            setLayout(new GridLayout(3,9));
+            addDummyPanels(9);
+            addDummyPanels(3);
+            add(exemplarButton);
+            add(communityButton);
+            add(contributorButton);
+            addDummyPanels(3);
+            addDummyPanels(9);
+
+        }
+
+        void addDummyPanels(int j){
+            for(int i = 0; i < j; i++){
+                JPanel newPanel = new JPanel();
+                newPanel.setBackground(Color.LIGHT_GRAY);
+                add(newPanel);
+            }
+        }
+    }
+
+    private class CMenuBar extends JMenuBar{
+        private JMenu menu = new JMenu("Menu");
+        private JMenuItem importItem = new JMenuItem("Import");
+        CMenuBar(){
+            menu.add(importItem);
+            add(menu);
+
+        }
+    }
 }
