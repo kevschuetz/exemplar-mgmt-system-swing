78a604b549cb0405811541f8b28e3a248c49f970
kevschuetz <kevin.hotz@gmx.at>
2021-07-11 11:50:58 +0200

    new branch commit

---
+++ database/src/main/java/resource/Community.java
@@ -0,0 +1,77 @@
+package resource;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class Community {
+    @Id
+    private String name;
+
+    @ManyToMany
+    private List<Exemplar> exemplars;
+
+    @ManyToMany
+    private List<User> members;
+
+    @ManyToOne
+    private User creator;
+
+
+    public Community(){};
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Exemplar> getExemplars() {
+        return exemplars;
+    }
+
+    public void setExemplars(List<Exemplar> exemplars) {
+        this.exemplars = exemplars;
+    }
+
+    public List<User> getMembers() {
+        return members;
+    }
+
+    public void setUsers(List<User> members) {
+        this.members = members;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public String toString(){
+        return this.name;
+    }
+
+    public void setMembers(List<User> members) {
+        this.members = members;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Community)) return false;
+        Community community = (Community) o;
+        return getName() != null && getName().equals(community.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName());
+    }
+}
